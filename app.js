36b526e46ce429adfa3370758779d368544dc56e
yassin <yassabdelkader22@gmail.com>
2026-08-04 18:25:19 +0200

    2

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -35,7 +33,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json('error');
 });
 
-module.exports = app;
+
